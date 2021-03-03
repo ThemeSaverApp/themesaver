e6551ebd01556696abf6e9d65d8ff0f9206909c2
RPICoder <68472305+techcoder20@users.noreply.github.com>
2021-03-03 10:04:56 +0530

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -6,7 +6,10 @@ rm ~/.local/share/applications/ThemeSaver.desktop
 #Removing icon
 sudo rm /usr/share/icons/ThemeSaver.png
 
-#Removing xfce4-panel-profiles
+#Removing themesaver file
+sudo rm /usr/local/bin/themesaver
+
+#Removing xfce4-panel-profiles and other dependencies
 sudo apt -y purge xfce4-panel-profiles xdotool python-pil.imagetk python3-pil python3-pil.imagetk fonts-ubuntu
 sudo apt install -f
 
