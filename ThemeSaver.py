5732709492db8589f8c9a5be6713823f6a3496b3
techcoder20 <techcoder20@gmail.com>
2022-02-04 15:55:52 +0530

    Add Shop Tutorial

--- ThemeSaver.py
+++ ThemeSaver.py
@@ -419,6 +419,8 @@ def export(slotname, filepath):
     # Cleaning Up
     os.system(f'rm -rf {filepath}/"{slotname}"')
 
+    os.system(f'cp {SlotsFolder}/"{slotname}"/info.json {filepath}/info.json')
+
     click.echo(click.style('Slot Exported To: ', fg='green') + click.style(f'{filepath}/{slotname}.tar.gz', fg='blue'))
 
 
